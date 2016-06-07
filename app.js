52d1c63299872df2ec956bb22f7e77293e783067
seevee <cmichaelvincent@gmail.com>
2016-06-06 21:17:02 -0600

    + Added mongoose

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/odiebotnet');
+//var monk = require('monk');
+//var db = monk('localhost:27017/odiebotnet');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +26,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use(function(req,res,next){
+//  req.db = db;
+//  next();
+//});
 
 app.use('/', routes);
 app.use('/users', users);
@@ -54,6 +64,10 @@ app.use(function(err, req, res, next) {
     message: err.message,
     error: {}
   });
+});
+
+app.listen(8080, function() {
+	console.log('odiebotnet has started on port 8080');
 });
 
 
