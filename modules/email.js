fcc406d510c223a143875ebc69a520b8f8d5cc32
Keith Nygaard <knygaard@kenzan.com>
2016-06-08 10:21:30 -0600

    + Added homeIpAddress endpoint in emails that sends email template with ipAddres

--- modules/email.js
+++ modules/email.js
@@ -5,14 +5,21 @@ var Mailer = (function() {
   var transporter = nodemailer.createTransport('smtps://odieabotnet%40gmail.com:odieabodie@smtp.gmail.com');
   var mailOptions = {
     from: '"OdieBotNet" <odieabotnet@gmail.com>',
-    to: 'keithanygaard@gmail.com, cmichaelvincent@gmail.com',
+    to: 'keithanygaard@gmail.com',
     subject: 'Test',
     text: 'Test nodemailer',
     html: '<p>Test nodemailer</p>'
   };
 
-  var sendTestMail = function () {
+// , cmichaelvincent@gmail.com 
 
+  var sendTestMail = function ( ipAddress ) {
+	// TODO: render email html
+	// TODO: Build mailOptions on the fly (N number of recipients
+
+    mailOptions.subject = "New Ip Address: " + ipAddress;
+
+    console.log( "Sending new adobie IP address: " + ipAddress );
     transporter.sendMail(mailOptions, function(error, info){
       if(error){
         return console.log(error);
--- routes/emails.js
+++ routes/emails.js
@@ -7,8 +7,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/test', function(req, res, next) {
-  Mailer.sendTestMail();
+router.post('/homeIpAddress/:ipAddress', function(req, res, next) {
+  Mailer.sendTestMail( req.param("ipAddress") );
   res.send('sending test email');
 });
 
